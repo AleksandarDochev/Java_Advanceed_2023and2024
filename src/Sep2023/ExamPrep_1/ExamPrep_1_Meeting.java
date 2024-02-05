4120fbcbbf3fb252d9e7049fe631d7ecadc24f02
Alex <aleksandaredochev@gmail.com>
2024-02-05 20:25:58 +0200

    Exam Prep meeting finsihed

--- src/Sep2023/ExamPrep_1/ExamPrep_1_Meeting.java
+++ src/Sep2023/ExamPrep_1/ExamPrep_1_Meeting.java
@@ -1,8 +1,6 @@
 package Sep2023.ExamPrep_1;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ExamPrep_1_Meeting {
@@ -10,8 +8,81 @@ public class ExamPrep_1_Meeting {
         Scanner scanner =new Scanner(System.in);
         String male = scanner.nextLine();
         String female = scanner.nextLine();
-        List<String> maleList = Arrays.stream(male.split(" ")).collect(Collectors.toList());
-        List<String> femaleList = Arrays.stream(female.split(" ")).collect(Collectors.toList());
+        List<Integer> maleList = Arrays
+                .stream(male.split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        Collections.reverse(maleList);
+        maleList.removeIf(number -> number.equals(0));
+        for (int i = 0; i < maleList.size(); i++) {
+            int currentIndexValue = maleList.get(i);
+            if (currentIndexValue%25==0){
+                maleList.remove(i);
+                maleList.remove(i++);
+            }
+        }
+        ArrayDeque<Integer> stackMale = new ArrayDeque<>();
+        for (int e:
+             maleList) {
+            stackMale.offer(e);
+        }
+
+        List<Integer> femaleList = Arrays
+                .stream(female.split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        femaleList.removeIf(number -> number.equals(0));
+        for (int i = 0; i < femaleList.size(); i++) {
+            int currentIndexValue = femaleList.get(i);
+            if (currentIndexValue%25==0){
+                femaleList.remove(i);
+                femaleList.remove(i++);
+            }
+        }
+        ArrayDeque<Integer> stackFemale = new ArrayDeque<>();
+
+        for (int e:
+                femaleList) {
+            stackFemale.offer(e);
+        }
+        int matchesCount=0;
+
+        while (!stackMale.isEmpty() && !stackFemale.isEmpty()){
+            if (stackMale.peek()==stackFemale.peek()){
+                stackMale.pop();
+                stackFemale.pop();
+                matchesCount++;
+            } else if (stackMale.peek()!=stackFemale.peek()) {
+                int maleCurrent=stackMale.peek();
+                maleCurrent= maleCurrent-2;
+                stackMale.pop();
+                if (maleCurrent>0){
+                    stackMale.addFirst(maleCurrent);
+                }
+                stackFemale.pop();
+            }
+        }
+
+        System.out.println("Matches: "+matchesCount);
+        StringJoiner joiner = new StringJoiner(", ");
+        if (stackMale.size()==0){
+            System.out.println("Males left: none");
+        }else {
+            for (Integer element : stackMale) {
+                joiner.add(element.toString());
+            }
+            // Print the result
+            System.out.println("Males left: " + joiner.toString());
+        }
+        if (stackFemale.size()==0){
+            System.out.println("Females left: none");
+        }else {
+            for (Integer element : stackFemale) {
+                joiner.add(element.toString());
+            }
+            // Print the result
+            System.out.println("Females left: " + joiner.toString());
+        }
 
     }
 }
