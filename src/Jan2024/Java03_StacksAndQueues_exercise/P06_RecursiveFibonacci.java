984bedacf1b5de7a847038addd5c18252e9e1a4f
Alex <aleksandaredochev@gmail.com>
2024-02-05 20:49:13 +0200

    stack and quest fibbonaci

--- src/Jan2024/Java03_StacksAndQueues_exercise/P06_RecursiveFibonacci.java
+++ src/Jan2024/Java03_StacksAndQueues_exercise/P06_RecursiveFibonacci.java
@@ -1,16 +1,29 @@
 package Jan2024.Java03_StacksAndQueues_exercise;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class P06_RecursiveFibonacci {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
-        long result = (n);
-        System.out.println(result);
+        Deque<Long> stack = new ArrayDeque<>();
+        stack.push(0L);
+        stack.push(1L);
+
+
+        if (n <2 ){
+            System.out.println(1);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            long num1 = stack.pop();
+            long num2 = stack.pop();
+            stack.push(num1);
+            stack.push(num1+num2);
+        }
+        System.out.println(stack.pop());
 
     }
-    //private static long fib(int n){
-//
-    //}
 }
