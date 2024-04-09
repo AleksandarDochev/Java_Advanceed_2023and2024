c0322dfc0c4275b17f267b1de51bbdc06e08b946
Alex <aleksandaredochev@gmail.com>
2024-04-09 13:53:39 +0300

    Java Advanced Exam - 13 December 2023

---
+++ src/Jan2024/Exam2023December13/T01_WormsAndHoles.java
@@ -0,0 +1,7 @@
+package Jan2024.Exam2023December13;
+
+public class T01_WormsAndHoles {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Jan2024/Exam2023December13/T02_TheGambler.java
@@ -0,0 +1,4 @@
+package Jan2024.Exam2023December13;
+
+public class T02_TheGambler {
+}
